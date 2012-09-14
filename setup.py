8f45dc6648842e2040514419409f75b5a5d11d81
Adam Terrey <arterrey@pretaweb.com>
2012-09-14 11:10:38 +1000

    initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "cronsingleton",
+    version = "1.0",
+    author = "Adam Terrey",
+    author_email = "arterrey@pretaweb.com",
+    description = ("A cron scheduler for a singleton configured by the commandline "),
+    license = "BSD",
+    keywords = "",
+    url = "http://packages.python.org/cronsingleton",
+    packages=['cronsingleton', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+    ],
+    install_requires=["apscheduler"],
+    entry_points={
+        'console_scripts': ['cronsingleton = cronsingleton.script:main']
+        }
+)
+
